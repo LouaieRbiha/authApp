c5299888e4240587c0a31678ca4cd09f5291dc63
Louaie Rbiha <louaierbiha@gmail.com>
2025-02-17 09:39:18 +0100

    test: add testing to auth service

--- src/app/auth/services/auth.service.spec.ts
+++ src/app/auth/services/auth.service.spec.ts
@@ -1,16 +1,68 @@
-import { TestBed } from '@angular/core/testing';
-
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
+import { createServiceFactory, SpectatorService } from '@ngneat/spectator';
+import { fakeDb } from '../utils/fakeDb';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let spectator: SpectatorService<AuthService>;
+  const createService = createServiceFactory(AuthService);
+  const mockRouter = { navigate: jest.fn() };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthService);
+    jest.clearAllMocks();
+    spectator = createService({
+      providers: [{ provide: Router, useValue: mockRouter }],
+    });
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(spectator.service).toBeTruthy();
+  });
+
+  describe('login', () => {
+    it('should login successfully with valid credentials', () => {
+      const { username, password, role } = fakeDb[0];
+
+      spectator.service.login({ username, password }).subscribe((res) => {
+        expect(spectator.service.loggedUser).toBeNull();
+        expect(res).toEqual({
+          success: true,
+          message: `${username} is logged in successfully`,
+        });
+
+        expect(spectator.service.loggedUser).toEqual({ username, role });
+      });
+    });
+
+    it('should not login with invalid credentials', () => {
+      const invalidUser = {
+        username: 'wrongUsername',
+        password: 'wrongPassword',
+      };
+
+      spectator.service
+        .login({
+          username: invalidUser.username,
+          password: invalidUser.password,
+        })
+        .subscribe((res) => {
+          expect(spectator.service.loggedUser).toBeNull();
+          expect(res).toEqual({
+            success: false,
+            message: `Invalid credentials`,
+          });
+
+          expect(spectator.service.loggedUser).toEqual(null);
+        });
+    });
+  });
+
+  describe('logout', () => {
+    it('should clear loggedUser and navigate to /auth/login', () => {
+      spectator.service.logout();
+
+      expect(spectator.service.loggedUser).toBe(null);
+      expect(mockRouter.navigate).toHaveBeenCalledWith(['/auth/login']);
+    });
   });
 });
