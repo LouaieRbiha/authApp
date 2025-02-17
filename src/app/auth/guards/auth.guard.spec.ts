484204d017cc1a37d8686b89b175c8ccade63fad
Louaie Rbiha <louaierbiha@gmail.com>
2025-02-17 09:38:27 +0100

    test: add testing to auth guard

--- src/app/auth/guards/auth.guard.spec.ts
+++ src/app/auth/guards/auth.guard.spec.ts
@@ -1,17 +1,40 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 
 import { authGuard } from './auth.guard';
+import { AuthService } from '../services/auth.service';
+import { MockService } from 'ng-mocks';
+import { Role } from '../models/user.interface';
 
 describe('authGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+  const executeGuard: CanActivateFn = (...guardParameters) =>
+    TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+
+  const authServiceMock: AuthService = MockService(AuthService);
+  const mockRouter = { navigate: jest.fn() };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        authGuard,
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: Router, useValue: mockRouter },
+      ],
+    });
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('should allow the route', () => {
+    authServiceMock.loggedUser = { username: 'aaaa', role: Role.USER };
+    expect(executeGuard({} as any, {} as any)).toBe(true);
+  });
+
+  it('should redirect to /auth/login', () => {
+    authServiceMock.loggedUser = null;
+    executeGuard({} as any, {} as any);
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/auth/login']);
   });
 });
--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = () => {
   const router = inject(Router);
   const loggedUser = inject(AuthService).loggedUser;
   return loggedUser ? true : router.navigate(['/auth/login']);
