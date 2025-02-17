a8632422b34141b0eccfbbc24e56a341ecae730d
Louaie Rbiha <louaierbiha@gmail.com>
2025-02-17 09:38:55 +0100

    test: add testing to common guard

--- src/app/auth/guards/common.guard.spec.ts
+++ src/app/auth/guards/common.guard.spec.ts
@@ -1,17 +1,39 @@
 import { TestBed } from '@angular/core/testing';
-import { CanActivateFn } from '@angular/router';
-
+import { CanActivateFn, Router } from '@angular/router';
 import { commonGuard } from './common.guard';
+import { AuthService } from '../services/auth.service';
+import { MockService } from 'ng-mocks';
+import { Role } from '../models/user.interface';
 
 describe('commonGuard', () => {
-  const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => commonGuard(...guardParameters));
+  const executeGuard: CanActivateFn = (...guardParameters) =>
+    TestBed.runInInjectionContext(() => commonGuard(...guardParameters));
+
+  const authServiceMock: AuthService = MockService(AuthService);
+  const mockRouter = { navigate: jest.fn() };
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        commonGuard,
+        { provide: AuthService, useValue: authServiceMock },
+        { provide: Router, useValue: mockRouter },
+      ],
+    });
   });
 
   it('should be created', () => {
     expect(executeGuard).toBeTruthy();
+  });
+
+  it('should allow to go to auth/login route', () => {
+    authServiceMock.loggedUser = null;
+    expect(executeGuard({} as any, {} as any)).toBe(true);
+  });
+
+  it('should redirect to /dashboard', () => {
+    authServiceMock.loggedUser = { username: 'aaaa', role: Role.USER };
+    executeGuard({} as any, {} as any);
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/dashboard']);
   });
 });
--- src/app/auth/guards/common.guard.ts
+++ src/app/auth/guards/common.guard.ts
@@ -2,7 +2,7 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
-export const commonGuard: CanActivateFn = (route, state) => {
+export const commonGuard: CanActivateFn = () => {
   const router = inject(Router);
   const loggedUser = inject(AuthService).loggedUser;
   return loggedUser !== null ? router.navigate(['/dashboard']) : true;
